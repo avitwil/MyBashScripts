b691b993c7e4bf6847621c54005578bc791ef99a
avitwil <avitwil@gmail.com>
2025-05-07 21:12:14 +0300

    Update scripttocommand.sh

--- scripttocommand.sh
+++ scripttocommand.sh
@@ -3,15 +3,28 @@
 if [  -z "$1"  ]
 then
      echo "missing script file path"
+     echo " correct format : sudo stc [script] [comand] [sudo/nothing]"
+     echo "  sudo stc /home/user/script.sh command "
+     echo "                or "
+     echo "  sudo stc /home/user/script.sh command sudo "
 elif ! [ -e $1 ]
 then
      echo "file $1 not exist"
+     echo " correct format : sudo stc [script] [comand] [sudo/nothing]"
+     echo "  sudo stc /home/user/script.sh command "
+     echo "                or "
+     echo "  sudo stc /home/user/script.sh command sudo "
 elif [  -z "$2"  ]
 then
      echo "missing command name"
+     echo " correct format : sudo stc [script] [comand] [sudo/nothing]"
+     echo "  sudo stc /home/user/script.sh command "
+     echo "                or "
+     echo "  sudo stc /home/user/script.sh command sudo "
 elif [ -e /usr/bin/$2 ]
 then
      echo "command $2 allready exist"
+     echo "try enother command name"
 elif [  -n "$3" -a "$3" != "sudo" ]
 then
      echo "write sudo or nothing"
